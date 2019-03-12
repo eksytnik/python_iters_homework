5aaa43b15963cd2dc0dcc10659daf43855a567b0
eksytnik <eksytnik@yandex.ru>
2019-03-12 14:32:53 +0300

    Tracing function updated for new rewuirements.

--- tracefunc.py
+++ tracefunc.py
@@ -1,6 +1,32 @@
 import sys
 
-def tracefunc(frame, event, arg):
-    if event == "return":
-        print(f"function: {frame.f_code.co_name} , local vars: {list(frame.f_locals.keys())}")
-    return tracefunc
+
+def tracefoo(function_to_trace):
+    """
+    Tracing function, takes function to trace as an argument.
+    Prints function name and local variables when the function is called.
+    """
+
+    def tracing(frame, event, *args):
+        if function_to_trace.__name__ == frame.f_code.co_name and event == "return":
+            print(f"function: {frame.f_code.co_name} , local vars: {list(frame.f_locals.keys())}")
+        return tracing
+
+    sys.settrace(tracing)
+
+
+def trace_decorator(function_to_trace, *args):
+    """
+    Tracing decorator, prints function name and local arguments when the function is called.
+    """
+
+    def inner_trace(*args):
+        def tracing(frame, event, *args):
+            if function_to_trace.__name__ == frame.f_code.co_name and event == "return":
+                print(f"function: {frame.f_code.co_name} , local vars: {list(frame.f_locals.keys())}")
+            return tracing
+
+        sys.settrace(tracing)
+        return function_to_trace(*args)
+
+    return inner_trace
