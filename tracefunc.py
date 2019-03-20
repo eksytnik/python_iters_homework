f6c639a3864c11bd44532e2cab9c9b1419388dcf
eksytnik <eksytnik@yandex.ru>
2019-03-20 10:55:39 +0300

    Required changes applied.

--- tracefunc.py
+++ tracefunc.py
@@ -1,7 +1,7 @@
 import sys
 
 
-def tracefoo(function_to_trace):
+def tracefoo(function_to_trace, *args):
     """
     Tracing function, takes function to trace as an argument.
     Prints function name and local variables when the function is called.
@@ -13,20 +13,5 @@ def tracefoo(function_to_trace):
         return tracing
 
     sys.settrace(tracing)
-
-
-def trace_decorator(function_to_trace, *args):
-    """
-    Tracing decorator, prints function name and local arguments when the function is called.
-    """
-
-    def inner_trace(*args):
-        def tracing(frame, event, *args):
-            if function_to_trace.__name__ == frame.f_code.co_name and event == "return":
-                print(f"function: {frame.f_code.co_name} , local vars: {list(frame.f_locals.keys())}")
-            return tracing
-
-        sys.settrace(tracing)
-        return function_to_trace(*args)
-
-    return inner_trace
+    function_to_trace(*args)
+    sys.settrace(None)
